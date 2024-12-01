ad6e7c9fd8a82e3d2204342a0303fe1ed8444a59
geek <elmainiaymane03@gmail.com>
2024-12-01 05:58:48 +0100

    working in progress in the lexer i finished the string handling
    next step is handling numbers

---
+++ src/com/wora/jlox/Lexer.java
@@ -0,0 +1,124 @@
+package com.wora.jlox;
+
+import static com.wora.jlox.TokenType.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Lexer {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    private int start;
+    private int current;
+    private int line = 1;
+
+    public Lexer(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEof()) {
+            start = current;
+            scanToken();
+        }
+
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken() {
+        char c = nextChar();
+        switch (c) {
+            case '(' -> addToken(LEFT_PAREN);
+            case ')' -> addToken(RIGHT_PAREN);
+            case '{' -> addToken(LEFT_BRACE);
+            case '}' -> addToken(RIGHT_BRACE);
+            case ',' -> addToken(COMMA);
+            case '.' -> addToken(DOT);
+            case '-' -> addToken(MINUS);
+            case '+' -> addToken(PLUS);
+            case ';' -> addToken(SEMICOLON);
+            case '*' -> addToken(STAR);
+
+            case '!' -> addToken(match('=') ? BANG_EQUAL : BANG);
+            case '=' -> addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+            case '<' -> addToken(match('=') ? LESS_EQUAL : LESS);
+            case '>' -> addToken(match('=') ? GREATER_EQUAL : GREATER);
+
+            case '/' -> {
+                if (match('/')) {
+                    skipComment();
+                    return;
+                }
+                addToken(SLASH);
+            }
+
+            case '"' -> string();
+            case '\t', '\r', ' ' -> {
+            }
+            case '\n' -> line++;
+
+            // TODO: create a global error handler
+            default -> System.out.println("Unexpected character");
+        }
+    }
+
+    private void string() {
+        while (peek() != '"' && !isAtEof()) {
+            if (peek() == '\n')
+                line++;
+            nextChar();
+        }
+
+        if(isAtEof()) {
+            //TODO: Create a globa error handler
+            System.out.println("String is not complete");
+            return;
+        }
+
+        nextChar();
+        String value = source.substring(start + 1, current -1);
+        addToken(STRING, value);
+    }
+
+    /*
+     * this method used to detect if there is two charcters
+     */
+    private boolean match(char expected) {
+        if (source.charAt(current) != expected)
+            return false;
+
+        current++;
+        return true;
+    }
+
+    private void addToken(TokenType type) {
+        tokens.add(new Token(type, "", null, line));
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private char nextChar() {
+        current++;
+        return source.charAt(current - 1);
+    }
+
+    private boolean isAtEof() {
+        return current >= source.length();
+    }
+
+    private void skipComment() {
+        while (peek() != '\n' && !isAtEof())
+            nextChar();
+    }
+
+    private char peek() {
+        if (isAtEof())
+            return '\0';
+        return source.charAt(current);
+    }
+}
---
+++ src/com/wora/jlox/Main.java
@@ -0,0 +1,47 @@
+package com.wora.jlox;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+class Main {
+    private static boolean hadError = false;
+
+    public static void main(String[] args) throws Exception {
+        if (args.length > 1) {
+            System.err.println("Usage: jlox file.lox");
+        } else if (args.length == 1) {
+            readFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void readFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+    }
+
+    private static void runPrompt() {
+        try (Scanner scanner = new Scanner(System.in)) {
+            while(true) {
+                System.out.print(">> ");
+                String line = scanner.nextLine();
+                if(line == null) break;
+
+                run(line);
+            }
+        }
+    }
+
+    private static void run(String source) {
+        System.out.println("source " + source);
+        Lexer lexer = new Lexer(source);
+        List<Token> tokens = lexer.scanTokens();
+        System.out.println("tokens list");
+        System.out.println(tokens);
+    }
+}
